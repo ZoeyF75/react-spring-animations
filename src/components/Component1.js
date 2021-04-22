5a39de0530f804b3107cb8c16d539b022a1ba12b
ZoeyF75 <zf.zoodle@gmail.com>
2021-04-22 15:23:14 -0700

    Counter setup, number's not working

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -1,7 +1,8 @@
 import { animated, useSpring } from 'react-spring';
 
 export default function Component1() {
-  const props = useSpring( { from: { opacity: 0, marginTop: -500}, to: { opacity: 1, marginTop: 0 } })
+  const props = useSpring( { from: { opacity: 0, marginTop: -500}, to: { opacity: 1, marginTop: 0 } });
+  const propsTimer = useSpring( { from: { number: 0 }, to: { number: 10 } } );
   return (
    <animated.div style={props}>
      <div style={c1Style}>
@@ -12,6 +13,11 @@ export default function Component1() {
         commodi itaque voluptates vel suscipit assumenda soluta ipsa
         voluptatibus laudantium labore harum?
       </p>
+        <animated.div style={propsTimer}>
+          <h1 style={counter}>
+            {/* {propsTimer.number} */}
+          </h1>
+        </animated.div>
     </div>
    </animated.div>
   ); 
@@ -21,4 +27,12 @@ const c1Style = {
   background: "steelblue",
   color: "white",
   padding: "1.5rem"
+};
+
+const counter = {
+  background: "#333",
+  textAlign: "center",
+  width: "100px",
+  borderRadius: "50%",
+  margin: "1rem auto"
 };
