4ae964c4012f1205fe9fb01f46f499a37d46b9d1
ZoeyF75 <zf.zoodle@gmail.com>
2021-04-21 18:47:47 -0700

    Component 1 animation working with new version of spring

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -1,25 +1,19 @@
-import { Spring } from "react-spring";
+import { animated, useSpring } from 'react-spring'
 
 export default function Component1() {
+  const props = useSpring( { from: { opacity: 0, marginTop: -500}, to: { opacity: 1, marginTop: 0 } })
   return (
-    <Spring
-      from={{ opacity: 0, marginTop: -500 }}
-      to={{ opacity: 1, marginTop: 0 }}
-    >
-      {props => (
-        <div style={props}>
-          <div style={c1Style}>
-            <h1>Component 1</h1>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
-              nobis adipisci eum minima deserunt at porro, veritatis officia
-              commodi itaque voluptates vel suscipit assumenda soluta ipsa
-              voluptatibus laudantium labore harum?
-            </p>
-          </div>
-        </div>
-      )}
-    </Spring>
+   <animated.div style={props}>
+     <div style={c1Style}>
+      <h1>Component 1</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
+        nobis adipisci eum minima deserunt at porro, veritatis officia
+        commodi itaque voluptates vel suscipit assumenda soluta ipsa
+        voluptatibus laudantium labore harum?
+      </p>
+    </div>
+   </animated.div>
   );
 }
 
