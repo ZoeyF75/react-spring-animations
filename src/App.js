c172084d88f21c96edef5ab09eef96c130a5c0d1
ZoeyF75 <zf.zoodle@gmail.com>
2021-04-24 00:55:54 -0700

    Transition needs to be updated for current react-spring version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Transition, animated } from 'react-spring';
 import './App.css';
 import Component1 from './components/Component1';
 import Component2 from './components/Component2';
@@ -9,14 +10,29 @@ class App extends Component {
     showComponent3: false
   }
 
-  toggle = e => console.log(123);
-  
+  toggle = e => this.setState( { showComponent3: !this.state.showComponent3 })
+
   render() {
     return (
       <div className="App">
         <Component1 />
         <Component2 toggle={this.toggle} />
-        <Component3 />
+        <Transition
+          native
+          items={this.state.showComponent3}
+          from={{ opacity: 0 }}
+          enter={{ opacity: 1 }}
+          leave={{ opacity: 0 }}
+        >
+          {show =>
+            show &&
+            (props => (
+              <animated.div style={props}>
+                <Component3 />
+              </animated.div>
+            ))
+          }
+        </Transition>
       </div>
     );
   }
