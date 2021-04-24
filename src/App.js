10a311800251b73ecf2c241febf19f3c65e240fe
ZoeyF75 <zf.zoodle@gmail.com>
2021-04-24 00:13:23 -0700

    Component 3 created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import Component1 from './components/Component1';
 import Component2 from './components/Component2';
+import Component3 from './components/Component3';
 
 function App() {
+  const state = {
+    showComponent3: false
+  }
+
   return (
     <div className="App">
       <Component1 />
---
+++ src/components/Component3.js
@@ -0,0 +1,21 @@
+const Component3 = () => {
+  return (
+    <div style={c3Style}>
+      <h1>Component 3</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
+          nobis adipisci eum minima deserunt at porro, veritatis officia
+          commodi itaque voluptates vel suscipit assumenda soluta ipsa
+          voluptatibus laudantium labore harum?
+        </p>
+    </div>
+  )
+}
+
+export default Component3;
+
+const c3Style = {
+  background: "skyblue",
+  color: "white",
+  padding: "1.5rem"
+};
